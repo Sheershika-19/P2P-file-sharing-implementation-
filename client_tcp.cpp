cc6cc1c3731b3b3776df0f0b202107509cd99d16
Sheershika-19 <sheershikathotli19@gmail.com>
2025-07-20 22:15:20 +0530

    Made some changes in implementation

--- client_tcp.cpp
+++ client_tcp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include <thread>
 #include <sstream>
 #include <cstring>
@@ -15,171 +16,201 @@ using namespace std;
 #define PEER_SERVER_PORT 5555  
 #define BUFFER_SIZE 1024
 
+void handle_peer_connection(int new_socket) {
+    char buffer[BUFFER_SIZE] = {0};
+    read(new_socket, buffer, BUFFER_SIZE);
+    string filename(buffer);
+
+    ifstream file(filename, ios::binary);
+    if (!file) {
+        cerr << "[ERROR] File not found: " << filename << endl;
+        string error_msg = "ERROR: File not found\n";
+        send(new_socket, error_msg.c_str(), error_msg.size(), 0);
+    } else {
+        char file_buffer[BUFFER_SIZE];
+        while (!file.eof()) {
+            file.read(file_buffer, BUFFER_SIZE);
+            send(new_socket, file_buffer, file.gcount(), 0);
+        }
+        cout << "[INFO] Sent file to peer: " << filename << endl;
+    }
+    close(new_socket);
+}
+
 void peer_server() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Peer server socket failed");
+        perror("[ERROR] Peer server socket failed");
         exit(EXIT_FAILURE);
     }
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PEER_SERVER_PORT);
+
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("Peer server bind failed");
+        perror("[ERROR] Peer server bind failed");
         exit(EXIT_FAILURE);
     }
+
     if (listen(server_fd, 3) < 0) {
-        perror("Peer server listen failed");
+        perror("[ERROR] Peer server listen failed");
         exit(EXIT_FAILURE);
     }
-    cout << "Peer (TCP) server started on port " << PEER_SERVER_PORT << endl;
+
+    cout << "[INFO] Peer server listening on port " << PEER_SERVER_PORT << endl;
+
     while (true) {
         if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("Peer server accept failed");
+            perror("[ERROR] Peer server accept failed");
             continue;
         }
-        memset(buffer, 0, BUFFER_SIZE);
-        read(new_socket, buffer, BUFFER_SIZE);
-        string filename(buffer);
-        ifstream file(filename, std::ios::binary);
-        if (!file) {
-            cerr << "File not found: " << filename << endl;
-            string error_msg = "ERROR: File not found\n";
-            send(new_socket, error_msg.c_str(), error_msg.size(), 0);
-        } else {
-            char file_buffer[BUFFER_SIZE];
-            while (!file.eof()) {
-                file.read(file_buffer, BUFFER_SIZE);
-                send(new_socket, file_buffer, file.gcount(), 0);
-            }
-        }
-        close(new_socket);
+        thread(handle_peer_connection, new_socket).detach();
     }
 }
- 
+
 bool fetch_file_from_peer(const string& peer_ip, const string& filename) {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
-        perror("Socket creation failed");
+        perror("[ERROR] Socket creation failed");
         return false;
     }
+
     struct sockaddr_in peer_addr{};
     peer_addr.sin_family = AF_INET;
     peer_addr.sin_port = htons(PEER_SERVER_PORT);
     inet_pton(AF_INET, peer_ip.c_str(), &peer_addr.sin_addr);
+
     if (connect(sock, (struct sockaddr*)&peer_addr, sizeof(peer_addr)) == -1) {
-        perror("Failed to connect to peer");
+        perror("[ERROR] Failed to connect to peer");
         close(sock);
         return false;
     }
+
     send(sock, filename.c_str(), filename.length(), 0);
-    cout << "Requesting file: " << filename << " from " << peer_ip << endl;
+    cout << "[INFO] Requesting file '" << filename << "' from peer: " << peer_ip << endl;
+
     char buffer[BUFFER_SIZE];
-    int bytes_received = read(sock, buffer, BUFFER_SIZE - 1);    
+    int bytes_received = read(sock, buffer, BUFFER_SIZE - 1);
+
     if (bytes_received <= 0) {
-        cout << "No response from peer: " << peer_ip << endl;
+        cout << "[WARN] No response from peer: " << peer_ip << endl;
         close(sock);
         return false;
-    }   
+    }
+
     buffer[bytes_received] = '\0';
     string response(buffer);
     if (response.find("ERROR: File not found") != string::npos) {
-        cout << "Peer " << peer_ip << " does not have the file.\n";
+        cout << "[INFO] Peer " << peer_ip << " does not have the file.\n";
         close(sock);
         return false;
     }
-    ofstream outfile("received_" + filename, std::ios::binary);
+
+    ofstream outfile("received_" + filename, ios::binary);
     if (!outfile) {
-        cout << "Failed to create output file: " << filename << endl;
+        cout << "[ERROR] Failed to create output file: " << filename << endl;
         close(sock);
         return false;
     }
+
     outfile.write(buffer, bytes_received);
     while ((bytes_received = read(sock, buffer, BUFFER_SIZE)) > 0) {
         outfile.write(buffer, bytes_received);
     }
-    cout << "File " << filename << " received successfully from peer " << peer_ip << endl;
+
+    cout << "[INFO] File '" << filename << "' received successfully from peer " << peer_ip << endl;
     outfile.close();
-    cout << "Connection to port 5555 closed\n\n";
     close(sock);
     return true;
 }
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <server_ip>\n";
+        cerr << "[ERROR] Usage: " << argv[0] << " <server_ip>\n";
         return 1;
     }
+
     string server_ip = argv[1];
     thread(peer_server).detach();
+
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_addr{};
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     inet_pton(AF_INET, server_ip.c_str(), &server_addr.sin_addr);
+
     if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Server connection failed");
+        perror("[ERROR] Server connection failed");
         return 1;
     }
-    cout << "Enter commands: "<<endl;
+
+    cout << "[INFO] Connected to central server at " << server_ip << endl;
+    cout << "[INFO] Enter commands:\n";
+
     string command;
     while (true) {
         getline(cin, command);
-        if(command.rfind("Register ", 0) == 0){
+
+        if (command.rfind("Register ", 0) == 0) {
             send(sock, command.c_str(), command.length(), 0);
             char response[BUFFER_SIZE];
             int bytes_read = read(sock, response, BUFFER_SIZE - 1);
             response[bytes_read] = '\0';
-            cout <<response << endl<< endl;
+            cout << "[SERVER] " << response << endl;
         }
+
         if (command.rfind("Send ", 0) == 0) {
-           string file_list = command.substr(5);  
-           istringstream ss(file_list);
-           string filename;
-           while (getline(ss, filename, ',')) {  
-               filename.erase(0, filename.find_first_not_of(" ")); 
-               filename.erase(filename.find_last_not_of(" ") + 1); 
-               string new_command = "Send " + filename;
-               send(sock, new_command.c_str(), new_command.length(), 0);  
-               char response[BUFFER_SIZE];
-               int bytes_read = read(sock, response, BUFFER_SIZE - 1);
-               response[bytes_read] = '\0';
-               if(response=="NOT FOUND"){
-                  cout<< "File "+filename+" not available"<<endl;
-               }
-               else {   
-               cout << "Peer details for "<< response;
-               istringstream iss(response);
-               string received_filename;
-               vector<string> peer_ips;
-               iss >> received_filename;        
-               string peer_ip;
-               while (iss >> peer_ip) {   
-                   peer_ips.push_back(peer_ip);
-               }
-               bool file_downloaded = false;
-               for (const auto& peer : peer_ips) {  
-                   if (!received_filename.empty() && !peer.empty()) {
-                       cout << "Trying peer: " << peer << " for file: " << received_filename << endl;
-                   if (fetch_file_from_peer(peer, received_filename)) {  
-                       file_downloaded = true;
-                       break;  
-                   } else {
-                      cout << "Failed to fetch from peer: " << peer << ", trying next...\n";
-                   }
-               }
+            string file_list = command.substr(5);
+            istringstream ss(file_list);
+            string filename;
+
+            while (getline(ss, filename, ',')) {
+                filename.erase(0, filename.find_first_not_of(" "));
+                filename.erase(filename.find_last_not_of(" ") + 1);
+                string new_command = "Send " + filename;
+                send(sock, new_command.c_str(), new_command.length(), 0);
+
+                char response[BUFFER_SIZE];
+                int bytes_read = read(sock, response, BUFFER_SIZE - 1);
+                response[bytes_read] = '\0';
+
+                if (string(response).find("NOT_FOUND") != string::npos) {
+                    cout << "[INFO] File '" << filename << "' not available\n";
+                } else {
+                    cout << "[INFO] Peer details for: " << response;
+                    istringstream iss(response);
+                    string received_filename, peer_ip;
+                    iss >> received_filename;
+                    vector<string> peer_ips;
+                    while (iss >> peer_ip) {
+                        peer_ips.push_back(peer_ip);
+                    }
+
+                    bool file_downloaded = false;
+                    for (const auto& peer : peer_ips) {
+                        if (!received_filename.empty() && !peer.empty()) {
+                            cout << "[INFO] Trying peer: " << peer << "...\n";
+                            if (fetch_file_from_peer(peer, received_filename)) {
+                                file_downloaded = true;
+                                break;
+                            } else {
+                                cout << "[WARN] Failed to fetch from peer: " << peer << "\n";
+                            }
+                        }
+                    }
+
+                    if (!file_downloaded) {
+                        cout << "[ERROR] Failed to download file from all peers.\n";
+                    }
+                }
             }
-           if (!file_downloaded) {
-              cout << "Failed to download from all available peers.\n";
-            }    
-          }     
-           continue;  
         }
-      } 
     }
+
     close(sock);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,64 +1,89 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <bits/stdc++.h>
 #include <thread>
 #include <mutex>
 #include <cstring>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <signal.h>
 
 using namespace std;
 
 #define SERVER_PORT 4444
 #define BUFFER_SIZE 1024
 
-unordered_map<string,vector<string>> file_db;
+unordered_map<string, vector<string>> file_db;
 mutex db_mutex;
+
+int server_fd = -1;  
+
+void handle_signal(int signum) {
+    cout << "\n[INFO] Caught signal " << signum << ", shutting down server..." << endl;
+    if (server_fd != -1) {
+        close(server_fd);  
+        cout << "[INFO] Closed server socket." << endl;
+    }
+    exit(0);  
+}
 
 void handle_client(int client_socket) {
     char buffer[BUFFER_SIZE];
     struct sockaddr_in peer_addr;
     socklen_t addr_len = sizeof(peer_addr);
     getpeername(client_socket, (struct sockaddr*)&peer_addr, &addr_len);
-    string peer_ip = inet_ntoa(peer_addr.sin_addr); 
+    string peer_ip = inet_ntoa(peer_addr.sin_addr);
+
     while (true) {
         memset(buffer, 0, BUFFER_SIZE);
         int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
         if (bytes_received <= 0) {
-            cout << "Client disconnected: " << peer_ip << "\n";
+            cout << "[INFO] Client disconnected: " << peer_ip << endl;
             close(client_socket);
             return;
         }
+
         string request(buffer);
-        request.erase(request.find_last_not_of(" \t\n\r") + 1); 
+        request.erase(0, request.find_first_not_of(" \t\n\r"));
+        request.erase(request.find_last_not_of(" \t\n\r") + 1);
+
         if (request.substr(0, 9) == "Register ") {
             string files = request.substr(9);
             lock_guard<mutex> lock(db_mutex);
             size_t pos = 0;
             while ((pos = files.find(',')) != string::npos) {
                 string filename = files.substr(0, pos);
-                file_db[filename].push_back(peer_ip);
+                auto& vec = file_db[filename];
+                if (find(vec.begin(), vec.end(), peer_ip) == vec.end()) {
+                    vec.push_back(peer_ip);
+                }
                 files.erase(0, pos + 1);
             }
             if (!files.empty()) {
-                file_db[files].push_back(peer_ip);
+                auto& vec = file_db[files];
+                if (find(vec.begin(), vec.end(), peer_ip) == vec.end()) {
+                    vec.push_back(peer_ip);
+                }
             }
 
             send(client_socket, "Registration successful\n", 23, 0);
         } else if (request.substr(0, 5) == "Send ") {
             string filename = request.substr(5);
             string response;
-
-            lock_guard<mutex> lock(db_mutex);
-            response = filename + " ";
-            if (file_db.find(filename) != file_db.end()) {
-                for (const auto& ip : file_db[filename]) {
-                    response += ip + " ";
+            {
+                lock_guard<mutex> lock(db_mutex);
+                auto it = file_db.find(filename);
+                response = filename + " ";
+                if (it != file_db.end()) {
+                    for (const auto& ip : it->second) {
+                        response += ip + " ";
+                    }
+                    response += "\n";
+                } else {
+                    response = "NOT_FOUND\n";
                 }
-                response += "\n";
-            } else {
-                response = "NOT_FOUND\n";
             }
             send(client_socket, response.c_str(), response.length(), 0);
         } else {
@@ -68,32 +93,41 @@ void handle_client(int client_socket) {
 }
 
 int main() {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    signal(SIGINT, handle_signal);  
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
         perror("Server socket failed");
         exit(EXIT_FAILURE);
     }
+
     struct sockaddr_in server_addr{};
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(SERVER_PORT);
+
     if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         perror("Bind failed");
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
+
     if (listen(server_fd, 5) == -1) {
         perror("Listen failed");
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
-    cout << "Server listening on port no." << SERVER_PORT << endl;
+
+    cout << "[INFO] Server listening on port " << SERVER_PORT << endl;
+
     while (true) {
         int client_socket = accept(server_fd, nullptr, nullptr);
         if (client_socket == -1) {
-            perror("Accept failed");
+            perror("[ERROR] Accept failed");
             continue;
         }
         thread(handle_client, client_socket).detach();
     }
-    close(server_fd);
-    return 0;
+
+    return 0;  
 }
